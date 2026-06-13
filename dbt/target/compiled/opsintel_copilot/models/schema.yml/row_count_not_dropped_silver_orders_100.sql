14d2e56ae92cadf2a259d542f9c69e16bc982d1d
Angad Deep Singh <singhangad2050@gmail.com>
2026-06-13 15:50:10 -0400

    Remove generated dbt folders from version control, add .gitignore

--- dbt/target/compiled/opsintel_copilot/models/schema.yml/row_count_not_dropped_silver_orders_100.sql
+++
@@ -1,4 +0,0 @@
-
-SELECT COUNT(*) AS row_count
-FROM `workspace`.`opsintel_copilot`.`silver_orders`
-HAVING COUNT(*) < 100
--- dbt/target/compiled/opsintel_copilot/models/staging/stg_admin_events.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    CAST(admin_event_id  AS STRING)    AS admin_event_id,
-    CAST(admin_user      AS STRING)    AS admin_user,
-    CAST(action          AS STRING)    AS action,
-    CAST(target_service  AS STRING)    AS target_service,
-    CAST(region          AS STRING)    AS region,
-    CAST(event_time      AS TIMESTAMP) AS event_time
-FROM `workspace`.`opsintel_copilot`.`bronze_admin_events`
-WHERE admin_event_id IS NOT NULL
